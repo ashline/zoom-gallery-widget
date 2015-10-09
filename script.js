bde854f7861d2ba04936efd7fd1658553a34a710
Ashline Mpofu <ashline.m@webinterpret.com>
2015-10-09 14:33:00 +0200

    added  source square

--- script.js
+++ script.js
@@ -5,15 +5,28 @@ $(document).ready(function() {
 console.log(imageHeight);
     var scaleFactorY = 5 - (400 / imageHeight);
     var scaleFactorX = 3;
-    $('#source').on('mousemove', function(e) {
-        var offsetY = e.offsetY;
-        var offsetX = e.offsetX;
+    var $square = $('#square');
+    var $sourceContainer =  $('#source-container');
+    var containerOffset = $sourceContainer.offset();
+    
+    function moveSquare(top, left) {
+        var width = $square.width();
+
+        $square.css({top:top,
+                          left: left});
+    }
+    $sourceContainer.on('mousemove', function(e) {
+        var offsetY = e.pageY - containerOffset.top;
+        var offsetX = e.pageX - containerOffset.left;
         var scaledOffsetX = scaleFactorX * offsetX;
         var scaledOffsetY = scaleFactorY * offsetY;
         
+        moveSquare(offsetY - $square.height()/2, offsetX - $square.width()/2);
         $preview.css('background-position', '-' + 
                      scaledOffsetX + 'px -' + scaledOffsetY +
                      'px');
-        console.log(scaledOffsetX, scaledOffsetY)
+//        console.log(scaledOffsetX, scaledOffsetY)
+    });
+    $square.on('mousemove', function() {
     });
 });
